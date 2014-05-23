90cdddd208bc823a391a7701351091a50d85aa66
iFollow Agência <renatoauto@ifollowagencia.com.br>
2014-05-23 15:09:17 -0400

    admin

--- wp-content/themes/WeRock/content-top.php
+++ wp-content/themes/WeRock/content-top.php
@@ -13,11 +13,16 @@ bread crums
           <div class="row">
               <div class="col-lg-6 col-md-6 col-sm-6">
                   <h1><?php 
-                  echo "Teste"
+
+                  the_title(); 
+                  echo the_title();
                   ?></h1>
                     <h5> <?php 
                               $saved_data = get_post_meta($post->ID,'xv_page-subtitle',true);
+                              
+
                               echo $saved_data;
+
 
                             ?>
                     </h5>
