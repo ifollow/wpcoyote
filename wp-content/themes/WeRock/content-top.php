7210b45eded500417449a3467f0f95ea39224976
iFollow Agência <renatoauto@ifollowagencia.com.br>
2014-05-23 15:12:02 -0400

    admin

--- wp-content/themes/WeRock/content-top.php
+++ wp-content/themes/WeRock/content-top.php
@@ -13,11 +13,8 @@ bread crums
           <div class="row">
               <div class="col-lg-6 col-md-6 col-sm-6">
                   <h1><?php 
-                  if (the_title() == "gallery") {
-                    echo "galeria"
-                  }
-                  //the_title(); 
-                 // echo the_title();
+                  
+                  the_title(); 
                   ?></h1>
                     <h5> <?php 
                               $saved_data = get_post_meta($post->ID,'xv_page-subtitle',true);
