f714a0587c6a2f0e32c54e033100144ac9890038
iFollow Agência <renatoauto@ifollowagencia.com.br>
2014-05-23 14:09:53 -0400

    admin

--- wp-content/themes/WeRock/content-single.php
+++ wp-content/themes/WeRock/content-single.php
@@ -20,7 +20,7 @@ Blog
                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                   <ul class="post-meta">
                       <li><span class="fa fa-clock-o"></span><?php the_time('j F, Y'); ?></li>
-                      <li><span class=" fa fa-user"></span><?php _e( 'by ', 'werock' ); ?><span><?php the_author(); ?></span></li>
+                      <li><span class=" fa fa-user"></span><?php _e( 'por ', 'werock' ); ?><span><?php the_author(); ?></span></li>
                       <li><span class="fa fa-comment"></span><?php comments_number( '0 Comentários', '1 Comentário', '% Comentários' ); ?></li>
                   </ul>
                     <?php the_content(); ?>
