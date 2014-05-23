9551229b9d4622b061e1f91f27d2202ccec5bbd7
iFollow Agência <renatoauto@ifollowagencia.com.br>
2014-05-23 15:14:03 -0400

    admin

--- wp-content/themes/WeRock/page-photo-gallery.php
+++ wp-content/themes/WeRock/page-photo-gallery.php
@@ -8,11 +8,29 @@ get_header();
 global $xv_data;
  ?>
 
+  <section class="breadcrumb">
+      
+       <div class="container">
+          <div class="row">
+              <div class="col-lg-6 col-md-6 col-sm-6">
+                  <h1>Galeria</h1>
+                    <h5> <?php 
+                              $saved_data = get_post_meta($post->ID,'xv_page-subtitle',true);
+                              
 
-<?php get_template_part( 'content', 'top' ); ?>
+                              echo $saved_data;
 
 
-
+                            ?>
+                    </h5>
+                </div>
+                
+                <div class="col-lg-6 col-md-6 col-sm-6">
+              <?php if (function_exists('werock_breadcrumbs')) { werock_breadcrumbs();} ?>
+          </div>
+       </div>
+  </section>
+<div class="clearfix"></div>
 
 
 <section id="gallery">
